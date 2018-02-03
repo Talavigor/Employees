f04499eb559e1e41055e636892e37221510a1562
Talavigor <talaviraigor@gmail.com>
2018-02-03 13:52:35 +0000

    Hierarchy

---
+++ resources/views/employees/count.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: korak
+ * Date: 06.02.2018
+ * Time: 20:59
+ */
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
             background-color: #fff;
             color: #636b6f;
             font-family: 'Raleway', sans-serif;
-            font-weight: 100;
+            /*font-weight: 100;*/
             height: 100vh;
             margin: 0;
         }
